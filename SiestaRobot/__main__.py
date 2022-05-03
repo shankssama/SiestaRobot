0a1238387b5aa75a9813cc9954fcad69142919aa
shankssama <98250680+shankssama@users.noreply.github.com>
2022-05-03 19:01:56 +0545

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -84,7 +84,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-SIESTA_IMG = "https://telegra.ph/file/6d3e3ef24f64fb6e35df0.jpg"
+SIESTA_IMG = "https://te.legra.ph/file/64104b32c98a8a050633b.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
  You can support the project by contacting @shanks_kun \
