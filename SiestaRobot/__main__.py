7bffdec785c7f04d303dcb9f51e10896986bda43
shankssama <98250680+shankssama@users.noreply.github.com>
2022-05-03 18:58:36 +0545

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -87,7 +87,7 @@ def get_readable_time(seconds: int) -> str:
 SIESTA_IMG = "https://telegra.ph/file/6d3e3ef24f64fb6e35df0.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
- You can support the project by contacting @saint_foire \
+ You can support the project by contacting @shanks_kun \
  Supporting isnt always financial! \
  Those who cannot provide monetary support are welcome to help us develop the bot at ."""
 
